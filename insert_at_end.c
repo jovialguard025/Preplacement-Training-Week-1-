c8dd69ac00ee4740061b6c0d35c27b8961e64491
jovialguard025 <MATHEWALBIN2005@GMAIL.COM>
2025-07-29 21:16:57 +0530

    The First One

---
+++ insert_at_end.c
@@ -0,0 +1,10 @@
+int largest(int arr[], int n) {
+     int max_element = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max_element) {
+            max_element = arr[i];
+        }
+    }
+    return max_element;
+}
+        
